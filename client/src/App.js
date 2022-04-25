8a33e7e09b6178e6f02cea3418039bf92836864c
mejerr <georgy_90@abv.bg~>
2022-04-25 19:33:29 +0300

    Chnag eapp button added

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import  React from 'react';
+import  React, { Fragment, useState } from 'react';
 import './theme-app/App.scss';
 import './App.css';
 
@@ -8,20 +8,37 @@ import Navbar from './theme-app/shared/Navbar';
 import Routes from './routes';
 
 const App = (props) => {
+  const [openApp, setOpenApp] = useState(false);
   const { isAuthenticated } = props;
+  let app;
+
+  if (openApp) {
+    app = (
+      <Fragment>
+        <Routes />
+      </Fragment>
+    );
+  } else {
+    app = (
+      <Fragment>
+        {isAuthenticated && <Sidebar/>}
+        <div className="container-fluid page-body-wrapper">
+          {isAuthenticated && <Navbar/>}
+          <div className="main-panel">
+            <div className="content-wrapper">
+              <Routes />
+            </div>
+            {isAuthenticated && <Footer/>}
+          </div>
+        </div>
+      </Fragment>
+    );
+  }
 
   return (
     <div className="container-scroller app-container">
-      {isAuthenticated && <Sidebar/>}
-      <div className="container-fluid page-body-wrapper">
-        {isAuthenticated && <Navbar/>}
-        <div className="main-panel">
-          <div className="content-wrapper">
-            <Routes />
-          </div>
-          {isAuthenticated && <Footer/>}
-        </div>
-      </div>
+      <div className="change-app-btn" onClick={() => setOpenApp(!openApp)}>Change app</div>
+      {app}
     </div>
   );
 };
